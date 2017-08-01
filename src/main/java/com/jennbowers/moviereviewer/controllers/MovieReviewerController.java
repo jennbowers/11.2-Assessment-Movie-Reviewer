a55ba3c369341937261327f4c01ecf301ab278b6
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-01 17:51:47 -0400

    CAN SUCCESSFULLY EDIT AND VIEW THE DETAIL FOR A SPECIFIC MOVIE! Fixed nav bar on all pages

--- src/main/java/com/jennbowers/moviereviewer/controllers/MovieReviewerController.java
+++ src/main/java/com/jennbowers/moviereviewer/controllers/MovieReviewerController.java
@@ -5,6 +5,7 @@ import com.jennbowers.moviereviewer.models.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,4 +37,38 @@ public class MovieReviewerController {
         repo.save(newMovie);
         return "redirect:/";
     }
+
+    @RequestMapping("/edit/{movieId}")
+    public String displayEditMovie(@PathVariable("movieId") Long movieId,
+                            Model model){
+        Movie movie = repo.findOne(movieId);
+        model.addAttribute("movie", movie);
+        return "editMovie";
+    }
+
+    @RequestMapping(value = "/edit/{movieId}", method = RequestMethod.POST)
+    public String editMovie(@PathVariable("movieId") Long movieId,
+                            @RequestParam("title") String title,
+                            @RequestParam("genre") String genre,
+                            @RequestParam("imdblink") String imdblink,
+                            @RequestParam("releasedate") String releasedate,
+                            Model model){
+        Movie movie = repo.findOne(movieId);
+        movie.setTitle(title);
+        movie.setGenre(genre);
+        movie.setImdbLink(imdblink);
+        movie.setReleasedate(releasedate);
+        repo.save(movie);
+        model.addAttribute("movie", movie);
+        return "detailMovie";
+    }
+
+    @RequestMapping("/movie/{movieId}")
+    public String displayDetailMovie(@PathVariable("movieId") Long movieId,
+                                     Model model) {
+        Movie movie = repo.findOne(movieId);
+        model.addAttribute("movie", movie);
+        return "detailMovie";
+    }
+
 }
