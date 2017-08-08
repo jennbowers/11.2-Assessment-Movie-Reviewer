50b8d3630406dbb16d9af9487f7342f3e81426fd
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-08 14:58:48 -0400

    updated models to reflect database

--- src/main/java/com/jennbowers/moviereviewer/models/Review.java
+++ src/main/java/com/jennbowers/moviereviewer/models/Review.java
@@ -8,25 +8,22 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String reviewername;
     private String rating;
-    private Integer reviewerage;
-    private String reviewergender;
-    private String revieweroccupation;
 
     @ManyToOne
     @JoinColumn(name = "movie_id")
     private Movie movie;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public Review() {}
 
-    public Review(String reviewername, String rating, Integer reviewerage, String reviewergender, String revieweroccupation, Movie movie) {
-        this.reviewername = reviewername;
+    public Review(String rating, Movie movie, User user) {
         this.rating = rating;
-        this.reviewerage = reviewerage;
-        this.reviewergender = reviewergender;
-        this.revieweroccupation = revieweroccupation;
         this.movie = movie;
+        this.user = user;
     }
 
     public long getId() {
@@ -37,14 +34,6 @@ public class Review {
         this.id = id;
     }
 
-    public String getReviewername() {
-        return reviewername;
-    }
-
-    public void setReviewername(String reviewername) {
-        this.reviewername = reviewername;
-    }
-
     public String getRating() {
         return rating;
     }
@@ -53,35 +42,19 @@ public class Review {
         this.rating = rating;
     }
 
-    public Integer getReviewerage() {
-        return reviewerage;
-    }
-
-    public void setReviewerage(Integer reviewerage) {
-        this.reviewerage = reviewerage;
-    }
-
-    public String getReviewergender() {
-        return reviewergender;
-    }
-
-    public void setReviewergender(String reviewergender) {
-        this.reviewergender = reviewergender;
-    }
-
-    public String getRevieweroccupation() {
-        return revieweroccupation;
-    }
-
-    public void setRevieweroccupation(String revieweroccupation) {
-        this.revieweroccupation = revieweroccupation;
-    }
-
     public Movie getMovie() {
         return movie;
     }
 
     public void setMovie(Movie movie) {
         this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/jennbowers/moviereviewer/models/User.java
+++ src/main/java/com/jennbowers/moviereviewer/models/User.java
@@ -1,4 +1,97 @@
 package com.jennbowers.moviereviewer.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user_data")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int age;
+    private String gender;
+    private String occupation;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user_data", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    public User() {}
+
+    public User(String name, int age, String gender, String occupation, String username, String password, List<Review> reviews) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+        this.username = username;
+        this.password = password;
+        this.reviews = reviews;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
